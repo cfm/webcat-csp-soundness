e9571288d8d4ad13511faef5bcb93af4452e28b1
Cory Francis Myers <cfm@panix.com>
2026-05-29 23:00:16 -0700

    refactor(Policy): per-directive validation clauses

--- policy.py
+++ policy.py
@@ -21,18 +21,26 @@ class Policy:
         return self._default_src
 
     @property
+    def default_src_valid(self):
+        # https://docs.webcat.tech/developers/CSP.html#default-src
+        return Or(self.default_src == SELF, self.default_src == NONE)
+
+    @property
     def object_src(self):
         return self._object_src
 
+    @property
+    def object_src_valid(self):
+        # https://docs.webcat.tech/developers/CSP.html#object-src
+        return Or(
+            self.object_src == NONE,
+            And(self.object_src == BOT, self.default_src == NONE),
+        )
+
     def valid(self):
         return And(
-            # https://docs.webcat.tech/developers/CSP.html#default-src
-            Or(self.default_src == SELF, self.default_src == NONE),
-            # https://docs.webcat.tech/developers/CSP.html#object-src
-            Or(
-                self.object_src == NONE,
-                And(self.object_src == BOT, self.default_src == NONE),
-            ),
+            self.default_src_valid,
+            self.object_src_valid,
         )
 
 
